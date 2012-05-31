32925a8a0744bea115cb06437b6475257d566fa1
Ole Weidner <ole.weidner@me.com>
2012-05-31 11:29:28 -0500

    fixed js error

--- js/commons.js
+++ js/commons.js
@@ -15,7 +15,7 @@ document.write(text);
 function forkme()
 {
  
-var text = '<a href="http://github.com/saga-project/bliss"><img class="c2" src="http://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" /></a> <script src=\'http://www.go2web20.net/twitterfollowbadge/1.0/badge.js\' type=\'text/javascript\'></script>
+var text = '<a href="http://github.com/saga-project/bliss"><img class="c2" src="http://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" /></a> <script src=\'http://www.go2web20.net/twitterfollowbadge/1.0/badge.js\' type=\'text/javascript\'></script>'
 
 document.write(text);
 }
